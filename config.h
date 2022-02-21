4fcb46d44f5184e7e07758b9b2b4d1b11e938aa4
Patrick-web <pntxall100@gmail.com>
2022-02-21 20:41:02 +0300

    Version 0.2

--- config.h
+++ config.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"Terminus:size=12", "FontAwesome:size=12"};
-static const char dmenufont[] = "Terminus:size=12";
-static const char col_gray1[] = "#222222";
+static const char *fonts[] = {"Terminus:size=10"};
+static const char dmenufont[] = "Terminus:size=10";
+static const char col_gray1[] = "#000000";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+static const char col_cyan[] = "#ff006e";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
@@ -19,7 +19,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"", "", "", "", "", "", "", "8", "9"};
+static const char *tags[] = {"", "", "", "", "", "", "ﴬ", "", ""};
 
 static const Rule rules[] = {
     /* xprop(1):
